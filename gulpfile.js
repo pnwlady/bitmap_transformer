143efce0d37a5d367720c0494954f64df86e3313
heidilaursen <heidimlaursen@gmail.com>
2016-07-12 11:20:19 -0700

    added files, set up gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const elsint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+var allFiles = [
+  '/lib/**/*',
+  '/test/**/*',
+  'gulpfile.js',
+  'package.json'
+];
+var testFiles = [
+  '/test/**/*.js'
+];
+
+gulp.task('lint', function() {
+  return gulp.src(allFiles)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('mocha', function() {
+  return gulp.src(testFiles)
+    .pipe(mocha());
+});
+
+gulp.task('default', ['lint', 'mocha']);
